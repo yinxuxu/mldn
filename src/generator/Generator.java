02a029a403c140ad1c6b05b47e2898acd6315226
yxx <15321937499@163.com>
2017-04-21 11:41:08 +0800

    11

---
+++ src/generator/Generator.java
@@ -0,0 +1,37 @@
+package generator;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+/**
+ *  
+ * <p>
+ * Title: Generator
+ * </p>
+ *  
+ * <p>
+ * Description: TODO(这里用一句话描述这个类的作用) 
+ * <p>
+ * <p>
+ * Company: www.itcast.com
+ * </p>
+ *  @author 传智.关云长   @date 2016-1-23 下午5:06:41    @version 1.0
+ */
+public class Generator {
+	public static void main(String[] args) throws Exception {
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		File configFile = new File("config/generatorConfig.xml");
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+				callback, warnings);
+		myBatisGenerator.generate(null);
+	}
+}
